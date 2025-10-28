08e34cfaef8030162dc461d612c4f2e099338021
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2025-10-28 19:18:54 +0000

    replace rsvp form with google-form

--- app/pages/rsvp.vue
+++ app/pages/rsvp.vue
@@ -1,126 +1,48 @@
 <template>
-  <v-container fluid>
-    <h1>RSVP</h1>
-    <p>
-      Kindly RSVP by 1/04/2026, as that's when we'll be closing our guest list.
-    </p>
-    <form @submit.prevent="submit">
-      <v-text-field
-        v-model="name.value.value"
-        :counter="10"
-        :error-messages="name.errorMessage.value"
-        label="Name"
-      ></v-text-field>
-      <v-radio-group
-        v-model="attendance.value.value"
-        label="Can you attend?"
-        :error-messages="attendance.errorMessage.value"
+  <v-container fluid class="rsvp">
+    <v-row>
+      <v-col justify="center">
+        <h1 class="text-center">RSVP</h1>
+        <p class="text-center">
+          Kindly RSVP by 1/04/2026, as that's when we'll be closing our guest
+          list.
+        </p>
+      </v-col>
+    </v-row>
+    <div class="form-wrapper">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSc-npJRoR6B5v1AkcYRK99y7-PpzKzzHzgufuX7Wf1LupIq8Q/viewform?embedded=true"
+        width="640"
+        height="1151"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        >Loading…</iframe
       >
-        <v-radio label="Yes, I wouldn't miss it" value="yes"></v-radio>
-        <v-radio label="No, I can't make it" value="no"></v-radio>
-      </v-radio-group>
-
-      <p class="pb-2">If yes, please provide your email:</p>
-      <v-text-field
-        v-model="email.value.value"
-        :error-messages="email.errorMessage.value"
-        label="E-mail"
-      ></v-text-field>
-      <v-radio-group
-        v-model="dietaryRequirements.value.value"
-        label="And do you have any dietary requirements?"
-        :error-messages="dietaryRequirements.errorMessage.value"
-      >
-        <v-radio label="No" :value="false"></v-radio>
-        <v-radio label="Yes" :value="true"></v-radio>
-      </v-radio-group>
-      <div v-if="dietaryRequirements.value.value">
-        <p>If yes, please add any dietary requirements:</p>
-        <v-checkbox
-          v-for="item in dietaryItems"
-          :key="item"
-          v-model="checkbox.value.value"
-          :label="item"
-          :value="item"
-          hide-details
-          :error-messages="checkbox.errorMessage.value"
-        ></v-checkbox>
-        <v-checkbox
-          v-model="checkbox.value.value"
-          label="Other"
-          value="Other"
-          hide-details
-        ></v-checkbox>
-        <v-text-field
-          v-model="otherDietary.value.value"
-          :counter="50"
-          label="Please specify other dietary requirements"
-          :disabled="!checkbox.value.value?.includes('Other')"
-        ></v-text-field>
-      </div>
-      <v-btn class="me-4" type="submit"> submit </v-btn>
-      <v-btn @click="handleReset"> clear </v-btn>
-    </form>
+    </div>
   </v-container>
 </template>
+<script lang="ts" setup></script>
 
-<script lang="ts" setup>
-import { ref } from "vue";
-import { useField, useForm } from "vee-validate";
+<style>
+.form-wrapper {
+  width: 100%;
+  max-width: 640px;
+  margin: 2rem auto;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
 
-const { handleSubmit, handleReset } = useForm({
-  validationSchema: {
-    name(value: string) {
-      if (value?.length >= 2) return true;
+.form-wrapper iframe {
+  width: 100%;
+  height: 1200px;
+  border: none;
+}
 
-      return "Name needs to be at least 2 characters.";
-    },
-    email(value: string) {
-      if (attendance.value) {
-        if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true;
-        return "Must be a valid e-mail.";
-      } else {
-        return true;
-      }
-    },
-    attendance(item: string) {
-      if (item) return true;
-      return "You must select an attendance option";
-    },
-    dietaryRequirements(item: boolean) {
-      if (item === undefined) return "You must select a dietary option";
-      return true;
-    },
-    checkbox(value: string[]) {
-      // Optional validation - remove if checkboxes are optional
-      if (value && value.length > 0) return true;
-      return true; // Set to true to make optional, or return error message to make required
-    },
-  },
-});
-
-const dietaryItems: Array<string> = [
-  "Vegetarian",
-  "Vegan",
-  "Pescatarian",
-  "Gluten-free",
-  "Dairy-free",
-  "Nut allergy",
-  "Shellfish allergy",
-];
-
-const name = useField("name");
-const email = useField("email");
-const attendance = useField("attendance");
-const dietaryRequirements = useField("dietaryRequirements");
-const checkbox = useField<string[]>("checkbox", undefined, {
-  initialValue: [], // Initialize as empty array for multi-select
-});
-const otherDietary = useField("otherDietary");
-
-const submit = handleSubmit((values) => {
-  alert(JSON.stringify(values, null, 2));
-});
-</script>
-
-<style></style>
+@media (max-width: 768px) {
+  .form-wrapper iframe {
+    height: 1400px;
+  }
+}
+</style>
